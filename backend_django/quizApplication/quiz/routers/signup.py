b40a6b905aec06a650085939dca5aaba56e0c685
Ketan Aggarwal <64477717+ketanAggarwal58@users.noreply.github.com>
2023-01-31 18:16:38 +0530

    signin and signup Api Created

--- backend_django/quizApplication/quiz/routers/signup.py
+++ backend_django/quizApplication/quiz/routers/signup.py
@@ -2,15 +2,23 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
 from quiz.serializers import signup
+from django.db import connections
 
 
-@api_view(['GET', 'POST'])
+@api_view(['POST'])
 def signUpViewSet(request):
     try:
+        cursor1 = connections['default'].cursor()
         parsedData = JSONParser().parse(request)
         data_serializer = signup.UserSerializer(data=parsedData)
         if data_serializer.is_valid():
-            sql = 'INSERT INTO User(firstName, lastName, email, phone, password) VALUES()'
+            with cursor1 as cursor:
+                sql = "INSERT INTO quiz_user(firstName, lastName, email, phone, password) VALUES('{0}', '{1}', '{2}', '{3}', '{4}')".format(
+                    data_serializer._validated_data.get('firstName'), data_serializer._validated_data.get('lastName'), data_serializer._validated_data.get('email'), data_serializer._validated_data.get('phone'), data_serializer._validated_data.get('password'))
+                for data in data_serializer._validated_data:
+                    print(data + " : " + data_serializer._validated_data.get(
+                        '{0}'.format(data)))
+                cursor.execute(sql)
             return Response({"msg": "Your Data Recieved Successfully!"})
         return Response({"msg": "Please fill all the data fields."})
     except:
