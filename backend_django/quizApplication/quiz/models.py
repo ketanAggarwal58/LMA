2ff1445fcfc06a419fa6f42707e86f9c64c7b010
Ketan Aggarwal <64477717+ketanAggarwal58@users.noreply.github.com>
2023-01-31 18:16:54 +0530

    minor bug fixed

--- backend_django/quizApplication/quiz/models.py
+++ backend_django/quizApplication/quiz/models.py
@@ -5,8 +5,26 @@ from django.db import models
 
 # User Model
 class User(models.Model):
-    fristName = models.CharField(max_length=256)
+    firstName = models.CharField(max_length=256)
     lastName = models.CharField(max_length=256)
     email = models.CharField(max_length=256)
-    phone = models.IntegerField()
+    phone = models.CharField(max_length=256)
     password = models.CharField(max_length=256)
+
+
+class Quiz(models.Model):
+    quizName = models.CharField(max_length=256)
+    quizQuestion = models.CharField(max_length=256)
+    quizAnswer1 = models.CharField(max_length=256)
+    quizAnswer2 = models.CharField(max_length=256)
+    quizAnswer3 = models.CharField(max_length=256)
+    quizAnswer4 = models.CharField(max_length=256)
+    correctAnswer = models.CharField(max_length=256)
+    questionScore = models.IntegerField()
+
+
+class quizSocre(models.Model):
+    quizName = models.CharField(max_length=256)
+    userEmail = models.CharField(max_length=256)
+    quizScore = models.IntegerField()
+    quizMaxScore = models.IntegerField()
